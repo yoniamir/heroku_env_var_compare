bfc8ad29cce972ee35a53d2b53c2f1a67e187bb1
Yoni Amir <yoni.amir3@gmail.com>
2021-06-26 00:49:14 +0300

    Check both ways

--- compare_envs.rb
+++ compare_envs.rb
@@ -12,7 +12,7 @@ env1_config = HerokuEnvConfig.new(ARGV[0].strip);nil
 env2_config = HerokuEnvConfig.new(ARGV[1].strip);nil
 
 def compare_hashes(hash1, hash2)
-  mismatched_keys = hash1.keys.reject { |k| hash1[k] == hash2[k] }
+  mismatched_keys = (hash1.keys + hash2.keys).uniq.reject { |k| hash1[k] == hash2[k] }
   mismatched_keys.each do |key|
     puts "key: #{key}"
     puts "hash1: #{hash1[key]}, hash2: #{hash2[key]}"
