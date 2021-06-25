db290f2697b92d9a446c00ce0c7192d7cc13bf8b
Yoni Amir <yoni.amir3@gmail.com>
2021-06-26 01:08:56 +0300

    More detailed output

--- compare_envs.rb
+++ compare_envs.rb
@@ -8,18 +8,21 @@ require 'pry'
 require 'json'
 require_relative 'heroku_env_config'
 
-env1_config = HerokuEnvConfig.new(ARGV[0].strip);nil
-env2_config = HerokuEnvConfig.new(ARGV[1].strip);nil
+puts "Comparing #{ARGV[0]} to #{ARGV[1]}..."
 
-def compare_hashes(hash1, hash2)
-  mismatched_keys = (hash1.keys + hash2.keys).uniq.reject { |k| hash1[k] == hash2[k] }
+env1_config = HerokuEnvConfig.new(ARGV[0]);nil
+env2_config = HerokuEnvConfig.new(ARGV[1]);nil
+
+def compare_hashes(env1, env2)
+  mismatched_keys = (env1.config.keys + env2.config.keys).uniq.reject { |k| env1.config[k] == env2.config[k] }
   mismatched_keys.each do |key|
     puts "key: #{key}"
-    puts "hash1: #{hash1[key]}, hash2: #{hash2[key]}"
+    puts "#{env1.env_name}: #{env1.config[key]}"
+    puts "#{env2.env_name}: #{env2.config[key]}"
     puts ""
   end
 
   puts "Total #{mismatched_keys.size} mismatches."
 end
 
-compare_hashes(env1_config.config, env2_config.config)
+compare_hashes(env1_config, env2_config)
