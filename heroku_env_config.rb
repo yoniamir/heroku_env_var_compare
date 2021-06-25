e0a9fd32665efaa593f78da6d694fc3b32e2cc1c
Yoni Amir <yoni.amir3@gmail.com>
2021-06-25 20:58:14 +0300

    Stash this before another approach

--- heroku_env_config.rb
+++ heroku_env_config.rb
@@ -6,25 +6,32 @@ require_relative 'clean_inspect'
 class HerokuEnvConfig
   include CleanInspect
 
-  attr_reader :env_name, :config_string
+  attr_reader :env_name, :config_string, :config
 
   def initialize(env_name)
     @env_name = env_name
     @config_string = fetch_config_string
-    @config = {}
+    @config = config_hash
     @excluded_inspect_vars = [:@config_string]
   end
 
-  def config
+  private
+
+  def config_hash
     hash = {}
+
     keys = config_string.scan(/\n([A-Z0-9_]+):/).flatten
-    keys.each do |key|
-      hash[key] = ''
+    keys_except_last = keys[0..-2]
+
+    keys_except_last.each do |key|
+      puts key
+      regex = /\n#{key}: +(.*)\n([A-Z0-9_]+):/
+      value = config_string.match(regex)[1]
+      hash[key] = value
     end
+
     hash
   end
-
-  private
 
   def fetch_config_string
     config_string = `heroku config -a #{env_name}`
