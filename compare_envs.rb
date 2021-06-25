d314517c2a3f1f54f4a12c5c18cda821ca4c681b
Yoni Amir <yoni.amir3@gmail.com>
2021-06-25 21:35:40 +0300

    Nice prints

--- compare_envs.rb
+++ compare_envs.rb
@@ -1,3 +1,4 @@
+# rubocop:disable all
 # frozen_string_literal: true
 
 # Example:
@@ -11,8 +12,11 @@ env1_config = HerokuEnvConfig.new(ARGV[0].strip);nil
 env2_config = HerokuEnvConfig.new(ARGV[1].strip);nil
 
 def compare_hashes(hash1, hash2)
-  hash1.keys.reject do |k|
-    hash1[k] == hash2[k]
+  mismatched_keys = hash1.keys.reject { |k| hash1[k] == hash2[k] }
+  mismatched_keys.each do |key|
+    puts "key: #{key}"
+    puts "hash1: #{hash1[key]}, hash2: #{hash2[key]}"
+    puts ""
   end
 end
 
