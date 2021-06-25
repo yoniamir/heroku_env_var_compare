b948ebeab578077c4463474a7cfe547f58fd906f
Yoni Amir <yoni.amir3@gmail.com>
2021-06-26 01:13:32 +0300

    Added colors

--- compare_envs.rb
+++ compare_envs.rb
@@ -7,8 +7,10 @@
 require 'pry'
 require 'json'
 require_relative 'heroku_env_config'
+require_relative 'string'
 
-puts "Comparing #{ARGV[0]} to #{ARGV[1]}..."
+puts "Comparing #{ARGV[0].magenta} to #{ARGV[1].cyan}..."
+puts ""
 
 env1_config = HerokuEnvConfig.new(ARGV[0]);nil
 env2_config = HerokuEnvConfig.new(ARGV[1]);nil
@@ -16,9 +18,9 @@ env2_config = HerokuEnvConfig.new(ARGV[1]);nil
 def compare_hashes(env1, env2)
   mismatched_keys = (env1.config.keys + env2.config.keys).uniq.reject { |k| env1.config[k] == env2.config[k] }
   mismatched_keys.each do |key|
-    puts "key: #{key}"
-    puts "#{env1.env_name}: #{env1.config[key]}"
-    puts "#{env2.env_name}: #{env2.config[key]}"
+    puts "key: #{key.bold}"
+    puts "#{env1.env_name.magenta}: #{env1.config[key]}"
+    puts "#{env2.env_name.cyan}: #{env2.config[key]}"
     puts ""
   end
 
